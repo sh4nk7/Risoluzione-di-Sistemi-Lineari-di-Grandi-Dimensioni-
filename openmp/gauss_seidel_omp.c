03b20207a62e221f8ce0e9baf8269d19cdc45c76
Giuseppe_Dimonte <84921881+sh4nk7@users.noreply.github.com>
2024-06-12 22:22:01 +0200

    gauss_seidel_omp.c

--- openmp/gauss_seidel_omp.c
+++ openmp/gauss_seidel_omp.c
@@ -1,75 +1,89 @@
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <omp.h>
 
-void load_data(double *A, double *b, int N) {
-    FILE *file_A = fopen("data/matrix_A.txt", "r");
-    FILE *file_b = fopen("data/vector_b.txt", "r");
+#define N 1000
+#define MAX_ITER 1000
+#define TOL 1e-6
 
-    if (file_A == NULL || file_b == NULL) {
-        perror("Errore nell'apertura dei file");
+void read_matrix(const char *filename, double A[N][N]) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening matrix file");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            fscanf(file_A, "%lf", &A[i * N + j]);
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            if (fscanf(file, "%lf", &A[i][j]) != 1) {
+                perror("Error reading matrix file");
+                fclose(file);
+                exit(EXIT_FAILURE);
+            }
         }
     }
-
-    for (int i = 0; i < N; i++) {
-        fscanf(file_b, "%lf", &b[i]);
-    }
-
-    fclose(file_A);
-    fclose(file_b);
+    fclose(file);
 }
 
-void parallel_gauss_seidel(double *A, double *b, double *x, int N, int max_iter, double tol) {
-    #pragma omp parallel
-    {
-        for (int iter = 0; iter < max_iter; iter++) {
-            #pragma omp for
-            for (int i = 0; i < N; i++) {
-                double sum = 0.0;
-                for (int j = 0; j < N; j++) {
-                    if (j != i) {
-                        sum += A[i * N + j] * x[j];
-                    }
+void read_vector(const char *filename, double b[N]) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening vector file");
+        exit(EXIT_FAILURE);
+    }
+
+    int i;
+    for (i = 0; i < N; i++) {
+        if (fscanf(file, "%lf", &b[i]) != 1) {
+            perror("Error reading vector file");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        }
+    }
+    fclose(file);
+}
+
+void parallel_gauss_seidel(double A[N][N], double b[N], double x[N]) {
+    int i, j, iter;
+    double norm, sum, x_new;
+
+    for (iter = 0; iter < MAX_ITER; iter++) {
+        norm = 0.0;
+
+        #pragma omp parallel for private(j, sum, x_new) reduction(+:norm)
+        for (i = 0; i < N; i++) {
+            sum = b[i];
+            for (j = 0; j < N; j++) {
+                if (j != i) {
+                    sum -= A[i][j] * x[j];
                 }
-                x[i] = (b[i] - sum) / A[i * N + i];
             }
+            x_new = sum / A[i][i];
+            norm += fabs(x_new - x[i]);
+            x[i] = x_new;
+        }
 
-            double norm = 0.0;
-            #pragma omp for reduction(+:norm)
-            for (int i = 0; i < N; i++) {
-                norm += (b[i] - A[i * N + i] * x[i]) * (b[i] - A[i * N + i] * x[i]);
-            }
-
-            if (norm < tol) break;
+        if (norm < TOL) {
+            break;
         }
     }
 }
 
 int main() {
-    int N = 1000;
-    double *A = (double *)malloc(N * N * sizeof(double));
-    double *b = (double *)malloc(N * sizeof(double));
-    double *x = (double *)malloc(N * sizeof(double));
-    
-    // Caricamento dei dati
-    load_data(A, b, N);
-    
-    // Inizializzazione del vettore soluzione
-    for (int i = 0; i < N; i++) {
-        x[i] = 0.0;
-    }
-    
-    parallel_gauss_seidel(A, b, x, N, 1000, 1e-6);
+    double A[N][N], b[N], x[N] = {0};
 
-    free(A);
-    free(b);
-    free(x);
-    
+    read_matrix("matrix.txt", A);
+    read_vector("vector.txt", b);
+
+    parallel_gauss_seidel(A, b, x);
+
+    // Print result
+    int i;
+    for (i = 0; i < N; i++) {
+        printf("%f\n", x[i]);
+    }
+
     return 0;
 }
